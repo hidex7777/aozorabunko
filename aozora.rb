2bc27f632441a508c12e5379e4bf73e0f4a4a76e
hidex7777@win <hidex7777@gmail.com>
2017-01-19 00:24:50 +0900

    till list of works output

--- aozora.rb
+++ aozora.rb
@@ -1,22 +1,42 @@
+# vim:set fileencoding=UTF-8
 require 'bundler'
 Bundler.require
 
-keyword = "江戸川乱歩"
+#姓と名の間に半角スペース
+keyword = "江戸川 乱歩"
+#「江戸川」のときは「あ行」。50音にないときは「他」。
 initial = "あ"
 top_url = "http://www.aozora.gr.jp/"
 
-dir_path = "./output"
+dir_path = "./output/"
 FileUtils.mkdir_p(dir_path) unless FileTest.exist?(dir_path)
 
-#「あ行」の作家のページに行く
-#江戸川乱歩のページに行く
-#公開中の全作品リストをゲットする。
 #公開中の全作品の文字数をゲットする
 #ID 作品名 文字数　という形式のTSVを出力
+$templine = Array.new(0)
 
 agent = Mechanize.new
 agent.user_agent_alias = 'Windows IE 9'
 top_page = agent.get(top_url)
 top_page.links_with(:href => /person/).each do |link|
-  puts link.href
+  #「あ行」の作家のページに行く
+  if link.text[initial] then
+    person_list = link.click
+    person_list.links_with(:href => /person/).each do |link|
+      #江戸川乱歩のページに行く
+      if link.text[keyword] then
+        listofworks = link.click
+        i = 0
+        listofworks.links_with(:href => /cards/).each do |link|
+          #公開中の全作品リストをゲットする。
+          t = link.text.encode("UTF-8")
+          $templine[i] = t
+          i+=1
+        end
+        File.open(dir_path + keyword + ".tsv", "w:UTF-8") do |f|
+          f.puts($templine)
+        end
+      end
+    end
+  end
 end
